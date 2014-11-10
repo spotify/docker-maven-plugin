f4f4e2dc118a7f8bcece0422acb2c795bf2f444f
David Xia <dxia@spotify.com>
2014-11-10 15:51:06 -0500

    Archive git commit hash
    Archive the git commit hash so we can tag it with a release number
    in downstream build phases.

--- create_artifacts.sh
+++ create_artifacts.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 # Tar up the staged release, all the pom files, and some build files. We will use these in
 # subsequent build steps to perform the actual release.
@@ -8,3 +8,5 @@ tar -zcvf target/docker-maven-plugin-staged-release.tar.gz `find . -name nexus-s
 VERSION=$(egrep -o '<version>.*</version>' -m 1 pom.xml | sed 's/<version>\(.*\)<\/version>/\1/')
 echo ${VERSION} > target/version
 
+# Output current git commit hash into file for later Jenkins items
+git rev-parse HEAD > target/commit_hash
