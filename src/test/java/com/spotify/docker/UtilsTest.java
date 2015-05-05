f199de136245733e334b982b2e4f7309930c50b1
Simon Weis <simon@w3is.de>
2015-05-05 21:52:13 +0200

    Fix nullpointer while reading settings.xml

--- src/test/java/com/spotify/docker/UtilsTest.java
+++ src/test/java/com/spotify/docker/UtilsTest.java
@@ -21,48 +21,72 @@
 
 package com.spotify.docker;
 
-import junit.framework.TestCase;
-
+import com.spotify.docker.client.AnsiProgressHandler;
+import com.spotify.docker.client.DockerClient;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.Log;
+import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
-import static com.spotify.docker.Utils.parseImageName;
+public class UtilsTest {
 
-public class UtilsTest extends TestCase {
+    private static final String TAG = "tag";
+    private static final String IMAGE = "image";
+    private static final String IMAGE_WITH_SEPERATOR = "image:";
+    private static final String IMAGE_WITH_LIBRARY = "library/image";
+    private static final String IMAGE_FROM_REGISTRY = "registry:80/library/image";
+    private static final String IMAGE_WITH_TAG = "image:tag";
+    private static final String IMAGE_FROM_LIB_WITH_TAG = "library/image:tag";
+    private static final String IMAGE_FROM_REG_WITH_TAG = "registry:80/library/image:tag";
 
-  public void testParseImageName() throws MojoExecutionException {
-    assertParsedCorrectly("registry.spotify.net/spotify/image", "tag",
-                          parseImageName("registry.spotify.net/spotify/image:tag"));
 
-    assertParsedCorrectly("registry:80/spotify/image", "tag",
-                          parseImageName("registry:80/spotify/image:tag"));
-
-    assertParsedCorrectly("spotify/image", "tag", parseImageName("spotify/image:tag"));
-
-    assertParsedCorrectly("image", "tag", parseImageName("image:tag"));
-
-    assertParsedCorrectly("image", null, parseImageName("image:"));
-
-    assertParsedCorrectly("image", null, parseImageName("image"));
-  }
-
-  private void assertParsedCorrectly(String expectedRepo,
-                                     String expectedTag,
-                                     String[] actualRepoTag) {
-    assertEquals("wrong repo", expectedRepo, actualRepoTag[0]);
-    assertEquals("wrong tag", expectedTag, actualRepoTag[1]);
-  }
-
-  public synchronized static ByteArrayInputStream getMockInputStream(String filename) {
-    try {
-      return new ByteArrayInputStream(Files.readAllBytes(Paths.get(filename)));
-    } catch (IOException e) {
-      throw new RuntimeException("Exception reading json stream from file", e);
+    @Test
+    public void testParseImageName() throws MojoExecutionException {
+        String[] result = Utils.parseImageName(IMAGE);
+        assertThat(result).containsExactly(IMAGE, null);
     }
-  }
 
+    @Test
+    public void testParseImageNameWithSeperator() throws MojoExecutionException {
+        String[] result = Utils.parseImageName(IMAGE_WITH_SEPERATOR);
+        assertThat(result).containsExactly(IMAGE, null);
+    }
+
+    @Test
+    public void testParseImageNameWithTag() throws MojoExecutionException {
+        String[] result = Utils.parseImageName(IMAGE_WITH_TAG);
+        assertThat(result).containsExactly(IMAGE, TAG);
+    }
+
+    @Test
+    public void testParseImageNameWithLibrary() throws MojoExecutionException {
+        String[] result = Utils.parseImageName(IMAGE_WITH_LIBRARY);
+        assertThat(result).containsExactly(IMAGE_WITH_LIBRARY, null);
+    }
+
+    @Test
+    public void testParseImageNameWithLibraryAndTag() throws MojoExecutionException {
+        String[] result = Utils.parseImageName(IMAGE_FROM_LIB_WITH_TAG);
+        assertThat(result).containsExactly(IMAGE_WITH_LIBRARY, TAG);
+    }
+
+    @Test
+    public void testParseImageNameFromRegistryAndTag() throws MojoExecutionException {
+        String[] result = Utils.parseImageName(IMAGE_FROM_REG_WITH_TAG);
+        assertThat(result).containsExactly(IMAGE_FROM_REGISTRY, TAG);
+    }
+
+    @Test
+    public void testPushImage() throws Exception {
+        DockerClient dockerClient = mock(DockerClient.class);
+        Log log = mock(Log.class);
+        Utils.pushImage(dockerClient, IMAGE, log);
+
+        verify(dockerClient).push(eq(IMAGE), any(AnsiProgressHandler.class));
+    }
 }
