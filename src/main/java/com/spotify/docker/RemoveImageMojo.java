5d102ef5debf4cbe657d887658174a507cb72dcb
aharon hacmon <aharon.topq@gmail.com>
2015-05-12 11:36:10 +0300

    fixing #94 Remove image mojo now supports multiple tags

--- src/main/java/com/spotify/docker/RemoveImageMojo.java
+++ src/main/java/com/spotify/docker/RemoveImageMojo.java
@@ -21,7 +21,12 @@
 
 package com.spotify.docker;
 
+import static com.google.common.base.Strings.isNullOrEmpty;
+import static com.spotify.docker.Utils.parseImageName;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -29,7 +34,6 @@ import org.apache.maven.plugins.annotations.Parameter;
 
 import com.spotify.docker.client.DockerClient;
 import com.spotify.docker.client.DockerException;
-import com.spotify.docker.client.DockerRequestException;
 import com.spotify.docker.client.shaded.javax.ws.rs.NotFoundException;
 import com.spotify.docker.client.ImageNotFoundException;
 
@@ -43,27 +47,31 @@ public class RemoveImageMojo extends AbstractDockerMojo {
     @Parameter(property = "imageName", required = true)
     private String imageName;
 
+    /** Additional tags to tag the image with. */
+    @Parameter(property = "dockerImageTags")
+    private List<String> imageTags;
+    
     protected void execute(DockerClient docker)
             throws MojoExecutionException, DockerException, IOException, InterruptedException {
-
-        getLog().info("Removing -f " + imageName);
-
-        try {
-            // force the image to be removed but don't remove untagged parents 
-            docker.removeImage(imageName, true, false);
-        } catch (Exception e) {
-            // ignoring 404 errors only
-            for (Throwable rootCause = e;
-                           rootCause != null && rootCause.getCause() != rootCause;
-                           rootCause = rootCause.getCause()) {
-               if (rootCause instanceof ImageNotFoundException || 
-                   rootCause instanceof NotFoundException) {
-                       getLog().warn("Image " + imageName + 
-                                     " does not exist and cannot be deleted - ignoring");
-                       return;
-                 }
-            }
-            throw e;
+        final String imageNameWithoutTag = parseImageName(imageName)[0];
+        if (imageTags == null){
+            imageTags = new ArrayList<String>();
+            imageTags.add("");
         }
+        for (final String imageTag : imageTags) {
+             String currImagName = imageNameWithoutTag +
+                          ((isNullOrEmpty(imageTag)) ? "" : ( ":"  + imageTag));
+             getLog().info("Removing -f " + currImagName); 
+             try {
+                    // force the image to be removed but don't remove untagged parents 
+                    docker.removeImage(currImagName, true, false);
+             } catch (ImageNotFoundException | NotFoundException e){
+                    // ignoring 404 errors only
+                    getLog().warn("Image " + imageName + 
+                        " does not exist and cannot be deleted - ignoring");
+                continue;
+             }
+        }
+         
     }
 }
